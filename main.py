46c40ba274181651c29d510442e881fd68f3c7b1
whaine.dev <whaine.dev@gmail.com>
2026-09-24 11:04:07 -0300

    agr tou fazendo com py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-x = 12
+x = int(input("Digete um valo: "))
 
-y = 24
+y = int(input("Digite outro valor: "))
 
 i = x + y
 
-print(f"Resuldado: {i}")
+print(f"Resuldado: i = 3 {i}")
+
+if i > 10:
+    print("i é maior que 10")
+else:
+    print("i não é maior que 10")
